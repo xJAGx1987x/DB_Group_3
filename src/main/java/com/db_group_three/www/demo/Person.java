e49bc1768abdae759709b3899034d32475490e4c
Joshua A. George <xjagx1987x@gmail.com>
2024-11-08 17:48:31 -0500

    corrected LoginApplication to include both username and password

---
+++ src/main/java/com/db_group_three/www/demo/Person.java
@@ -0,0 +1,90 @@
+package com.db_group_three.www.demo;
+
+public class Person {
+    private int personID;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    // Constructor
+    public Person(int personID, String name, String email, int phoneNumber, String address, String city, String state, int zipCode) {
+        this.personID = personID;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    // Getters and setters
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+}
+
