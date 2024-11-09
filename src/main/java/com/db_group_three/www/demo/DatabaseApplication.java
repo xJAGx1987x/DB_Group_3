696d1d437e36783a85ac24a91fefcfd811fe376b
Joshua A. George <xjagx1987x@gmail.com>
2024-11-09 13:43:42 -0500

    prog update

---
+++ src/main/java/com/db_group_three/www/demo/DatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.db_group_three.www.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DatabaseApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
