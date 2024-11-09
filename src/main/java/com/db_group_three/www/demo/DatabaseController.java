ad7399952705fcd2425a806ccf34e77589655104
Joshua A. George <xjagx1987x@gmail.com>
2024-11-09 18:18:37 -0500

    vehicle page prepped for logic

--- src/main/java/com/db_group_three/www/demo/DatabaseController.java
+++ src/main/java/com/db_group_three/www/demo/DatabaseController.java
@@ -2,56 +2,71 @@ package com.db_group_three.www.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class DatabaseController {
 
     @FXML
     private TextField makeField;
 
+
     @FXML
     private TextField modelField;
 
-    @FXML
-    private TextField yearField;
-
-    @FXML
-    private ChoiceBox<String> colorChoiceBox;
-
-    @FXML
-    private TextField priceField;
 
     @FXML
     private Button vSearchButton;
 
+
     @FXML
     private ListView<String> vehicleListView;
 
-    // Initialize method to set up the ChoiceBox and other components
     @FXML
+    private RadioButton yearRadioButton;
+
+
+    @FXML
+    private RadioButton monthRadioButton;
+
+
+    private ToggleGroup toggleGroup;
+
+
+    // Initialize method to set up the ToggleGroup and other components
+
+    @FXML
+
     public void initialize() {
-        // Populate the colorChoiceBox with options
-        colorChoiceBox.getItems().addAll("Red", "Blue", "Green", "Black", "White", "Silver", "Yellow");
+
+        // Create and set the ToggleGroup for the radio buttons
+
+        toggleGroup = new ToggleGroup();
+        yearRadioButton.setToggleGroup(toggleGroup);
+        monthRadioButton.setToggleGroup(toggleGroup);
+
+        // Optionally set a default selection
+        yearRadioButton.setSelected(true);
     }
+
 
     // Method to handle search button click
     @FXML
     private void handleSearchButtonAction() {
+
         String make = makeField.getText();
         String model = modelField.getText();
-        String year = yearField.getText();
-        String color = colorChoiceBox.getValue();
-        String price = priceField.getText();
+        String timePeriod = yearRadioButton.isSelected() ? "Yearly" : "Monthly";
+
+        System.out.println("Search pressed");
 
         // Placeholder logic to display search criteria in the ListView
         vehicleListView.getItems().clear();
         vehicleListView.getItems().add("Search Results:");
         vehicleListView.getItems().add("Make: " + make);
         vehicleListView.getItems().add("Model: " + model);
-        vehicleListView.getItems().add("Year: " + year);
-        vehicleListView.getItems().add("Color: " + color);
-        vehicleListView.getItems().add("Price: " + price);
+        vehicleListView.getItems().add("Time Period: " + timePeriod);
     }
 }
