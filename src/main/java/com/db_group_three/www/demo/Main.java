345da0c9f63dfec5d76c8966eccd914a2190c045
Joshua A. George <xjagx1987x@gmail.com>
2024-11-11 12:54:29 -0500

    added InventoryUpdater.java and altered main to accommodate

--- src/main/java/com/db_group_three/www/demo/Main.java
+++ src/main/java/com/db_group_three/www/demo/Main.java
@@ -1,8 +1,27 @@
 package com.db_group_three.www.demo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        // commented out the login and db applications
+        // so I could add code to push images to the DB.
+        // Sorry, I guess for being so fucking awesome.
         // LoginApplication.launch(LoginApplication.class, args);
-        DatabaseApplication.launch(DatabaseApplication.class, args) ;
+        // DatabaseApplication.launch(DatabaseApplication.class, args) ;
+
+        InventoryUpdater updater = new InventoryUpdater();
+        Scanner scanner = new Scanner(System.in);
+
+        // Collect inputs
+        System.out.print("Enter Vehicle ID (1-301): ");
+        int vehicleID = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        System.out.print("Enter Image File Path: ");
+        String filePath = scanner.nextLine();
+
+        // Call the method to update the inventory image
+        updater.updateInventoryImage(vehicleID, filePath);
     }
 }
